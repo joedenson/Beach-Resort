724189efaf76a2ec00703302bb79ab40918e595d
Joe Denson <joedenson79@gmail.com>
2019-10-22 17:27:26 -0700

    setup components

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                Hello from the Single Room page
+            </div>
+        )
+    }
+}
+
+export default SingleRoom;
+
