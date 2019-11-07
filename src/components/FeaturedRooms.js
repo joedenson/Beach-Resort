47de69910087f8b622c17fea10bc5ca13b380da2
Joe Denson <joedenson79@gmail.com>
2019-11-06 20:58:44 -0800

    added styled components dep

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { RoomContext } from '../context';
-
-
+import Loading from './Loading';
+import Room from './Room';
+import Title from '../Title';
 
 
 class FeaturedRooms extends React.Component {
     static contextType = RoomContext;
 
     render() {
-        const { name, greeting } = this.context;
-        return (
-            <div>
+        let { loading, featuredRooms: rooms } = this.context;
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })
 
-            </div>
-        )
+        return (
+            <section className='featured-rooms'>
+                <Title title='featured rooms' />
+                <div className='featured-rooms-center'>
+                    {loading ? <Loading /> : rooms}
+                </div>
+
+
+            </section>
+        );
     }
 }
 
